08b191df0782a2d4c5a5a7ad00a47766ddc31350
Andreas Kohler <andi@andi-makes.dev>
2022-05-28 18:35:04 +0200

    Added own Item Group
    Added simple own item group, need to change the icon though

--- src/main/java/dev/schmarrn/schmagie/common/Schmagie.java
+++ src/main/java/dev/schmarrn/schmagie/common/Schmagie.java
@@ -1,7 +1,9 @@
 package dev.schmarrn.schmagie.common;
 
+import net.minecraft.item.*;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.item.group.api.QuiltItemGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +17,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ToolMaterials;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -26,16 +24,17 @@ import net.minecraft.util.registry.Registry;
 public class Schmagie implements ModInitializer {
 	public static final String MOD_ID = "schmagie";
 	public static final String MOD_NAME = "Schmagie";
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-	public static final Item WOODEN_STAFF = new Staff(0, 0, ToolMaterials.WOOD, BlockTags.SHOVEL_MINEABLE, new Item.Settings().group(ItemGroup.MISC));
+
+	public static final ItemGroup ITEM_GROUP = QuiltItemGroup.builder(new Identifier(MOD_ID, "schmagie")).icon(() -> new ItemStack(Items.OBSIDIAN)).build();
+
+	public static final Item WOODEN_STAFF = new Staff(0, 0, ToolMaterials.WOOD, BlockTags.SHOVEL_MINEABLE, new Item.Settings().group(ITEM_GROUP));
 
 	public static final Block OBELISK = new Obelisk(AbstractBlock.Settings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(50.0F, 1200.0F));
 	public static BlockEntityType<ObeliskEntity> OBELISK_BLOCK_ENTITY;
 	public static final Block OBELISK_TOP = new ObeliskTop(AbstractBlock.Settings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(50.0F, 1200.0F));
+
 
 	private void registerItem(String name, Item item) {
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, name), item);
@@ -43,7 +42,7 @@ public class Schmagie implements ModInitializer {
 
 	private void registerBlock(String name, Block block) {
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, name), block);
-		registerItem(name, new BlockItem(block, new Item.Settings().group(ItemGroup.MISC)));
+		registerItem(name, new BlockItem(block, new Item.Settings().group(ITEM_GROUP)));
 	}
 
 	@Override
